69b3953aa036f0090dab7c7cf80d9fb2326e131b
Katrina Li <likatrina7@gmail.com>
2021-05-02 22:13:11 -0700

    Create 3 book routes

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,10 @@ def create_app(test_config=None):
 
     from .routes import hello_world_bp
     app.register_blueprint(hello_world_bp)
+
     from app.models.book import Book
+    from .routes import books_bp
+    app.register_blueprint(books_bp)
 
     db.init_app(app)
     migrate.init_app(app, db)
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,71 @@
-from flask import Blueprint
+from app import db
+from app.models.book import Book
+from flask import Blueprint,request,jsonify
 
+# bp_01
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+def is_int(value):
+    try:
+        return int(value)
+    except ValueError:
+        return False
+
+@books_bp.route("/<book_id>", methods=["GET", "PUT", "DELETE"], strict_slashes=False)
+def handle_single_book(book_id):
+    if not is_int(book_id):
+        return {
+        "message": f"Book id is must be integer.",
+        "success": False
+    }, 404
+
+    book = Book.query.get(book_id)
+    if not book:
+        return {
+        "message": f"Book id_{book_id} was not found.",
+        "success": False
+    }, 404
+    elif request.method == "GET":
+        return book.to_json(), 200
+    elif request.method == "PUT":
+        data_to_update = request.get_json()
+        book.title = data_to_update["title"]
+        book.description = data_to_update["description"]
+        db.session.commit()
+        return {
+            "success": True,
+            "message": f"Book id_{book_id} successfully updated"
+        }, 200
+    else:
+        db.session.delete(book)
+        db.session.commit()
+        return {
+            "success": True,
+            "message": f"Book id_{book_id} successfully deleted"
+        }, 200
+
+@books_bp.route('', methods=["GET"], strict_slashes=False)
+def get_all_books():
+    books = Book.query.all()
+    books_response = []
+    for book in books:
+        books_response.append(book.to_json())
+    return jsonify(books_response)
+
+@books_bp.route('', methods=["POST"], strict_slashes=False)
+def create_book():
+    request_body = request.get_json()
+    new_book = Book(title = request_body["title"],\
+        description = request_body["description"])
+
+    db.session.add(new_book)
+    db.session.commit()
+    return {
+        "success": True,\
+        "message": f"Book {new_book.title} has been created"
+        }, 201
+
+# bp_02
 hello_world_bp = Blueprint("hello_world", __name__)
 
 @hello_world_bp.route('/hello-world', methods=["GET"])
